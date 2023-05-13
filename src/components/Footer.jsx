f0b0e5b176327ef54b342dd15907014be57e7b6e
Rajan Ravisaheb <rajanroy220@gmail.com>
2023-05-13 16:58:35 +0530

    final changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+
+    
+    <div>
+        <h1>TechStar</h1>
+        <p>@all rights reserved - 2023</p>
+
+    </div>
+
+    <div>
+        <h5>Follow US</h5>
+        <div>
+        <a  target={"blank"}>
+            Youtube
+          </a>
+          <a  target={"blank"}>
+            Instagram
+          </a>
+          <a  target={"blank"}>
+            Github
+          </a>
+        </div>
+    </div>
+    </footer>
+  )
+}
+
+export default Footer
